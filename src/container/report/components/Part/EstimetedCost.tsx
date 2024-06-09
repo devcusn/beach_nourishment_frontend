ba3961af38c3bcd86e122eb43c82c539f9f15ea0
Halil Tunc <hitunc@outlook.com>
2024-06-10 01:16:35 +0300

    feat: estimeted cost add

--- src/container/report/components/Part/EstimetedCost.tsx
+++ src/container/report/components/Part/EstimetedCost.tsx
@@ -1,33 +1,51 @@
+import useProjectStore from "../../../../store/projectStore";
 import classes from "./style.module.css";
 const EstimatedCost = () => {
+  const { project, shoreLength, beachLength } = useProjectStore();
+  const volumeWithoutSill = project.volume.toFixed(2);
+  const volumeWithSill = (project.volume_detail.c1 * shoreLength).toFixed(2);
+  const sillDimension = (shoreLength * 1.5 * 1.5).toFixed(2);
+  const shoreLengthFixed = shoreLength.toFixed(2);
+  const beachLengthFixed = beachLength.toFixed(2);
+  const sillCost =
+    19 * Number(volumeWithSill) +
+    Number(sillDimension) * 4 +
+    2 * Number(beachLengthFixed) * 1 * 50;
+  const withoutSillCost =
+    19 * Number(volumeWithoutSill) + 2 * Number(shoreLengthFixed) * 1 * 50;
   return (
     <div className={classes.estimeted_cost_container}>
       <div>
-        In this project, we fill 146,444.33 m3 of sand without using sill and
-        49,866.2 m3 of sand with using sill in our project. We made an average
-        cost estimation for the operation, equipment, the cost of sand, sill,
-        groins, and labor using beach nourishment or regulations previously made
-        around the world.
+        In this project, we fill {volumeWithoutSill} m3 of sand without using
+        sill and {volumeWithSill}.2 m3 of sand with using sill in our project.
+        We made an average cost estimation for the operation, equipment, the
+        cost of sand, sill, groins, and labor using beach nourishment or
+        regulations previously made around the world.`
       </div>
       <div> The unit cost was $19 /m3 for sand </div>
       <div>
-        Unit cost was $42 /m3 for stone sill (our sill dimension = 200*1.5*1.5 =
-        450 m3)
+        Unit cost was $42 /m3 for stone sill (our sill dimension =
+        {shoreLength.toFixed(2)}
+        *1.5*1.5 = {sillDimension} m3)
       </div>
       <div>
-        Unit cost was $50 /m3 for the groin (our groin length is at least 87.5m,
-        we selected 100m)
+        Unit cost was $50 /m3 for the groin our groin length is at least
+        {beachLengthFixed}m
       </div>
       <div>
         **All unit costs include labor, operation, equipment, and material
         costs.
       </div>
-      <b>When we use sill our cost will be:</b>
+      <b>When we dont use sill our cost will be:</b>
       <div>
-        19* 49,866.2 +450*4+2*100*1*50= $412 ,189.8 + $1800 + $1000 = $950,257.8
+        19 * {volumeWithSill} + {sillDimension} * 4 + 2 * {beachLengthFixed} * 1
+        * 50 = ${sillCost.toFixed(2)}
       </div>
       <b>When we use sill our cost will be:</b>
-      <div>19* 146,444.33 +2*100*1*50= $2,782,442.3 + $1000 = $2,783,442.3</div>
+      <div>
+        19 * {volumeWithoutSill}+ 2 * {shoreLengthFixed} * 1 * 50= $2,782,442.3
+        + $1000 = ${withoutSillCost.toFixed(2)}
+      </div>
     </div>
   );
 };
